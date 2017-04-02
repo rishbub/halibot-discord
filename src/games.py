f81fb329039c1ead4007a0af7f0c935bb0efa772
rishbub <rishabhverma.rv@gmail.com>
2017-04-01 17:45:05 -0700

    Finish roll() and rollnumber() Create help tool.

--- src/games.py
+++ src/games.py
@@ -1,34 +1,117 @@
 import random
-import discord
 
-def roll(message):
-    questiontype = message.content.split()[1].lower()
-    if (questiontype == "who") and not ("or " in message.content):  # for 'who' questions without choices
-        return random.choice(list(message.server.members)).name
 
-    #this block is still in progress
-    if (questiontype == "which") or (questiontype == "who" and "or " in message.content):  # for any questions with choices
+def play(command, args, author, channel, server):
+    """Takes input from handler() and calls a function
+    play() always returns a string."""  # This alt text should not be viewed under help()
+    if command == "roll":
+        if args == "":
+            return ("?")
+        return roll(args, server.members)
+    if command == "rollnumber":
+        return rollnumber(args)
+
+
+def rollnumber(number):
+    """Input a number and roll a die with that many sides
+    (e.g. `><>rollnumber 6`)"""
+    try:
+        return str(random.choice(range(1, int(number)+1)))
+    except:
+        return "I don't like that number."
+
+
+def roll(question, members):
+    """Input a question and find out its answer.
+    (e.g. `><>roll who is my crush?`)"""
+
+    questiontype = question.split()[0].lower()
+
+    if "rish" in question:
+        if random.randint(1, 5) == 5:
+            return "Idk man i think rishy's pretty cool."  # eyy
+
+    # simple
+    if questiontype in "do-does-can-will-should-is-am-was-are-were":
+        return random.choice(default_responses)
+
+    # 'who'
+    if (questiontype[:3] == "who") and not ("or " in question):
+        return random.choice(list(members)).name
+
+    # choices with fluff
+    if (questiontype == "which") or ("or " in question and "who " in question):
         # delete the question stem, saves the rest of the question in $choices
-        separator = len(message.content)
-        for character in ",:?":
+        separator = len(question)
+        for character in ",:":
             try:
-                separator = min(message.content.index(character), separator)
-            except ValueError:
-                pass
-        if separator == len(message.content):
-            return "Sorry, I couldn't understand your question"
-        choices = message.content[separator + 1:]
+                separator = min(question.index(character), separator)
+            except ValueError: pass
+        # test break
+        if separator == len(question):
+            return "Sorry, I couldn't understand your question (couldn't separate)"
 
-        # turn choices into a list of options
-        choices = ''.join(x for x in choices if x.isalnum())  # delete all non-alphanumeric characters
+        choices = question[separator + 1:]
+        # turn choices from a string with fluff, into a list without fluff
+        choices = ''.join(x for x in choices if (x.isalnum() or ord(x) == 32))
         if "or " in choices:  # delete the word 'or'
-            choices.replace("or ", " ")
-        choices.split()
+            choices = choices.replace("or ", " ")
+        choices = choices.split()
 
+        # test break
         if choices == []:
-            return "Sorry, I couldn't understand your question"
+            return "Sorry, I couldn't understand your question (no choices left)"
+
         # pick a random one
         return random.choice(choices)
 
-    string = '''\n''' + message.content + '''\n''' +  '''\n''' + questiontype
-    return string
+    # choices separated by 'or'
+    if " or " in question:
+        choices = ''.join(x for x in question if (x.isalnum() or ord(x) == 32))
+        choices = choices.split(" or ")
+        return random.choice(choices)
+
+    # 'how many' TODO
+    if questiontype == "how":
+        if question.split()[1] in "do-does-can-will-should-is-am-was-are-were"):
+            pass
+        else:
+            return random.randint(1, 10*random.randint(1, random.randint(1, random.randint(1, 40)))) #gets a random number, more likely to be smaller but larger numbers are still possible
+
+    # test break
+    return random.choice(unsure_responses)
+
+
+default_responses = (
+            "Yes.",
+            "Certainly!",
+            "Mhm!",
+            "Yep.",
+            "You betcha!",
+            "Nope.",
+            "Definitely not.",
+            "Eww, no!"
+            "Why not?"
+            "Maybe.",
+            "Who knows?",
+            "Seems fishy.",
+            "That seems pretty fishy.",
+            "That's up for debate.",
+            "Probably.",
+            "Probably not.",
+            "ಠ_ಠ",  # look of disapproval
+            "( ͡° ͜ʖ ͡°)",
+            "¯\_(ツ)_/¯"
+        )
+
+unsure_responses = (
+    "Now, how would I know?",
+    "You tell me.",
+    "Beats me.",
+    "Hmm...",
+    "I dunno about that."
+    "me too thanks",
+    "Same"
+    "moi aussi",
+    "yo tambien"
+)
--- src/main.py
+++ src/main.py
@@ -1,10 +1,10 @@
 import discord
 import logging
 import games
-import os
 import asyncio
 
-# This block is copy-pasted from the docs
+# This 5-line block is copy-pasted from the discord.py docs
+# https://discordpy.readthedocs.io/en/latest/logging.html
 logger = logging.getLogger('discord')
 logger.setLevel(logging.DEBUG)
 handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
@@ -21,7 +21,6 @@ param.close()
 client = discord.Client()
 
 
-
 @client.event
 async def on_ready():
     print("Starting Halibot...")
@@ -30,11 +29,37 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
+    def channel_out(text):
+        return "client.send_message(message.channel,'"+text+"')"
+
     if message.content.startswith(command_phrase):
-        command = message.content.split()[0][3:]
-        if command == 'roll':
-            print(games.roll(message))
-            #await client.send_message(message.channel, games.roll(message))
+        command = message.content.split()[0][len(command_phrase):]  # find the command
+
+        try: args = message.content[message.content.index(" ") + 1:]  # finds the args if they exist
+        except: args = ""
+
+        if command == "help":
+            if args == "":  # for blank args
+                await client.send_message(message.channel, "**List of commands**\n" + "\n".join(command_list))
+
+            elif args == "[command]":  # for stupid args
+                await client.send_message(message.channel, "Don't you know what brackets stand for?")
+
+            elif len(args.split()) == 1:  # for correct args
+                try: await client.send_message(message.channel, args + ": "+ eval('games.' + args + '.__doc__'))
+
+                except AttributeError:
+                    await client.send_message(message.channel, "I don't recognize that function")
+
+            else:
+                await client.send_message(message.channel, "Sorry, I couldn't understand that")
+
+        elif command in command_list:
+            await client.send_message(message.channel, games.play(command, args, message.author, message.channel, message.server))
+        else:
+            await client.send_message(message.channel, "Try `><>help [command]`")
 
 
+
+command_list = ["roll", "rollnumber"]
 client.run(token)
